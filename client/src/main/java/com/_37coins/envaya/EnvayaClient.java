ca0e9745ddad0ef0117402fac81027352fe4d66b
sergey.sinev <xeodon@gmail.com>
2015-06-02 16:54:34 +0300

    #https://github.com/37coins/features/issues/96 fix NPE client consumers (gateway)

--- client/src/main/java/com/_37coins/envaya/EnvayaClient.java
+++ client/src/main/java/com/_37coins/envaya/EnvayaClient.java
@@ -24,6 +24,7 @@ import com.squareup.okhttp.Response;
 public class EnvayaClient {
     private static final Logger log = LoggerFactory.getLogger(EnvayaClient.class);
     private final OkHttpClient httpClient;
+    private final ObjectMapper objectMapper = new ObjectMapper();
     private String uri;
     private String digestToken;
     private String mobile;
@@ -42,7 +43,7 @@ public class EnvayaClient {
     protected <K> K parsePayload(Response response, Class<K> entityClass)
             throws EnvayaClientException {
         try {
-            return new ObjectMapper().readValue(response.body().byteStream(), entityClass);
+            return objectMapper.readValue(response.body().byteStream(), entityClass);
         } catch (IOException e) {
             log.error("envaya client parsePayload error:", e);
             throw new EnvayaClientException(
@@ -64,15 +65,16 @@ public class EnvayaClient {
                     EnvayaClientException.Reason.ERROR_GETTING_RESOURCE, e);
         }
 
-        log.debug("Response Status code:"+response.code());
-        if (isSucceed(response) && request.method().equals("DELETE")) {
-          return parsePayload(response, entityClass);
-        } else if (isSucceed(response)) {
-           return null;
-        } else {
-            throw new EnvayaClientException(
-                    EnvayaClientException.Reason.AUTHENTICATION_FAILED);
+        if (isSucceed(response)) {
+            if (request.method().equals("DELETE")) {
+                return null; // assume no any entity
+            }
+            return parsePayload(response, entityClass);
         }
+        // TODO: what about other status codes?!
+        log.warn("Response Status code: {}", response.code());
+        throw new EnvayaClientException(
+                    EnvayaClientException.Reason.AUTHENTICATION_FAILED);
     }
     
     protected <K> K getPayload(EnvayaRequest request, Class<K> entityClass)
